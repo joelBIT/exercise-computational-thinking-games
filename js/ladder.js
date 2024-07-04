5a02dfc5cc2fecfb7b276a7673ce6f7b8f67a15d
joelBit <joel.rollny@gmail.com>
2024-07-04 16:42:16 +0200

    Added comments to Ladder

--- js/ladder.js
+++ js/ladder.js
@@ -9,6 +9,9 @@ const die = document.querySelector(".die");
 let throws = 0;
 let expected = 1;
 
+/**
+ * Reset the game state.
+ */
 restartButton.addEventListener('click', () => {
     throwButton.disabled = false;
 
@@ -20,25 +23,25 @@ restartButton.addEventListener('click', () => {
     expected = 1;
 });
 
+/**
+ * Throw a die.
+ */
 throwButton.addEventListener('click', () => {
     throws++;
     throwsLabel.innerHTML = `Throws: ${throws}`;
 
-    let dots = Math.floor(Math.random() * 6) + 1;
+    let dots = throwDie();
     changeDieSide(dots);
 
-    if (dots === expected && expected < 6) {
-        expected++;
-        expectedLabel.innerHTML = `Expected: ${expected}`;
-    }
-
-    if (dots === expected && (expected === 6)) {
-        expectedLabel.innerHTML = 'GAME OVER';
-        expectedLabel.classList.add('finished');
-        throwButton.disabled = true;
-    }
+    checkIfLadderAchieved(dots);
 });
 
+/**
+ * Change the die side image to that of the thrown die.
+ * 
+ * @param {*} die the die element
+ * @param {*} dots the number of dots of the die side
+ */
 function changeDieSide(dots) {
     die.className = 'die';
 
@@ -62,4 +65,39 @@ function changeDieSide(dots) {
             die.classList.add('six');
             break;
     }
+}
+
+/**
+ * Simulates a die throw.
+ * 
+ * @returns a number between 1 and 6
+ */
+function throwDie() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+/**
+ * Compare dots with the expected value. If they are the same, increment the expected dot value. If both dots and the expected value
+ * is 6, then a ladder has been achieved and the game is over.
+ * 
+ * @param {*} dots the number of dots of the die in the most recent throw.
+ */
+function checkIfLadderAchieved(dots) {
+    if (dots === expected && expected < 6) {
+        expected++;
+        expectedLabel.innerHTML = `Expected: ${expected}`;
+    }
+
+    if (dots === expected && (expected === 6)) {
+        gameOver();
+    }
+}
+
+/**
+ * Inform the user that the game is over, and disable the throw button.
+ */
+function gameOver() {
+    expectedLabel.innerHTML = 'GAME OVER';
+    expectedLabel.classList.add('finished');
+    throwButton.disabled = true;
 }
