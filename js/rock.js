4a24e29142a3e21c037f9912b636bdf2dbce96ae
joelBit <joel.rollny@gmail.com>
2024-07-04 08:06:50 +0200

    Pseudocode for Rock Paper Scissors

---
+++ js/rock.js
@@ -0,0 +1,19 @@
+/**
+ * Pseudo code for Rock Paper Scissors
+ * 
+ * 
+ * 
+ * Target point is 3 (when the game ends)
+ * Loop until one player reach the target point
+ *      All players show their hand shape simultaneously (paper > rock && rock > scissors && scissors > paper)
+ *      if draw
+ *          continue
+ *      else
+ *          +1 point to the player that won the round
+ *          check if player have reached the target point
+ *              if true
+ *                  player wins the game, end game
+ *              else
+ *                  continue
+ * 
+ */
