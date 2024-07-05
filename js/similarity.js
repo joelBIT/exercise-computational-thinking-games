58b155ffac3fccaad8f79303c6e72e62563a2f41
joelBit <joel.rollny@gmail.com>
2024-07-05 07:53:23 +0200

    Changed the alert message in Word Similarity

--- js/similarity.js
+++ js/similarity.js
@@ -2,6 +2,7 @@
 
 const firstWord = prompt('Enter a word');
 const secondWord = prompt('Enter another word');
+const letters = [];
 
 let numberOfSimilarLetters = 0;
 let iterations = 0;
@@ -15,7 +16,8 @@ if (firstWord.length > secondWord.length) {
 for (let i = 0; i < iterations; i++) {
     if (firstWord[i] === secondWord[i]) {
         numberOfSimilarLetters++;
+        letters.push(firstWord[i]);
     }
 }
 
-alert(`There are ${numberOfSimilarLetters} letters occurring in the same position in both words`);
+alert(`There are ${numberOfSimilarLetters} letters (${letters}) occurring in the same position of the words ${firstWord} and ${secondWord}`);
